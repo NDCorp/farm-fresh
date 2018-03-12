bf60bb815c2b42374d8dda0144cc7d79e148d815
Amrinder Kalsi <amrinder.kalsi13@gmail.com>
2018-03-12 18:38:13 -0400

    Loaded Static Resources

--- farmfresh/src/main/java/com/netsky/farmfresh/controller/PageController.java
+++ farmfresh/src/main/java/com/netsky/farmfresh/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.netsky.farmfresh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +16,32 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to Spring Web MVC");
 		return mv;
 	}
+	
+//	@RequestMapping(value = "/test")
+//	public ModelAndView test(@RequestParam("greeting") String greeting) {
+//		
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+	
+//	@RequestMapping(value = "/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
+//		
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+	
+//	@RequestMapping(value = "/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting") String greeting) {
+//		
+//		if(greeting == null){
+//			greeting = "Hello World!";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
 
 }
