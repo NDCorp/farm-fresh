c457c968e7e72ab9b4ac8d2fd1aa3edc99708e3d
Amrinder Kalsi <amrinder.kalsi13@gmail.com>
2018-03-12 20:26:36 -0400

    Completed the interface theme.

--- farmfresh/src/main/java/com/netsky/farmfresh/controller/PageController.java
+++ farmfresh/src/main/java/com/netsky/farmfresh/controller/PageController.java
@@ -1,9 +1,7 @@
 package com.netsky.farmfresh.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,35 +11,28 @@ public class PageController {
 	public ModelAndView index() {
 		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickedHome", true);
 		return mv;
 	}
 	
-//	@RequestMapping(value = "/test")
-//	public ModelAndView test(@RequestParam("greeting") String greeting) {
-//		
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
-//	}
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickedAbout", true);
+		return mv;
+	}
 	
-//	@RequestMapping(value = "/test")
-//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
-//		
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
-//	}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickedContact", true);
+		return mv;
+	}
 	
-//	@RequestMapping(value = "/test/{greeting}")
-//	public ModelAndView test(@PathVariable("greeting") String greeting) {
-//		
-//		if(greeting == null){
-//			greeting = "Hello World!";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
-//	}
 
 }
